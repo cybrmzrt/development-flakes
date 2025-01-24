21e21630680ccb6012343bdc2d977cb3aa9c442c
Cybrmzrt <cybrmzrt@gmx.de>
2025-01-24 21:27:51 +0000

    Update python development flake.
    ➕ Add dependencies (cookiecutter, poetry, python3, mkdocs, refurb)

--- templates/python-dev/flake.nix
+++ templates/python-dev/flake.nix
@@ -26,7 +26,7 @@
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
           # Add any you need here
-          packages = with pkgs; [ ];
+          packages = with pkgs; [ cookiecutter poetry python3 mkdocs refurb ];
 
           # Set any environment variables for your dev shell
           env = { };
